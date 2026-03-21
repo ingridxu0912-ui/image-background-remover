89000812034a89f1e489627a49949fb5f33b6a66
ingridxu0912-ui <ingrid.xu0912@gmail.com>
2026-03-21 20:34:51 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  distDir: 'dist',
-  images: {
-    unoptimized: true,
-  },
-}
+  /** @type {import('next').NextConfig} */
+  const nextConfig = {
+    output: 'standalone',
+    images: {
+      unoptimized: true,
+    },
+  }
 
-module.exports = nextConfig
+  module.exports = nextConfig
